6d0a97e519443f073f8031e2ec4d5f5d00025994
Long Nguyen <long@dhcp-18-20-248-44.dyn.mit.edu>
2019-09-28 13:02:06 -0400

    [ps3] Translation vector definition

--- problem_sets/ps3/align.cpp
+++ problem_sets/ps3/align.cpp
@@ -125,6 +125,7 @@ Image alignAndDenoise(const vector<Image> &imSeq, int maxOffset)
     // // --------- HANDOUT  PS03 ------------------------------
     // Registers all images to the first one in a sequence and outputs
     // a denoised image even when the input sequence is not perfectly aligned.
+    vector<vector<int>> translations(imSeq.size() - 1, vector<int>{});
     return Image(1, 1, 1);
 }
 

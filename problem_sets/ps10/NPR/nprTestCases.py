9b54b2f39754649f78d2dcc853d3c0dc7ec38992
Long Nguyen <lpn@mit.edu>
2019-12-08 00:11:29 -0500

    [ps10] Added metrics for renders

--- problem_sets/ps10/NPR/nprTestCases.py
+++ problem_sets/ps10/NPR/nprTestCases.py
@@ -10,7 +10,7 @@ import random as rnd
 import numpy as np
 import nprHelper as nprh
 import math
-
+from time import time
 
 '''
 nprScript.py contains some basic test cases.
@@ -42,18 +42,35 @@ forest = io.imread('forest.png')
 catalina = io.imread('catalina.png')
 
 
+'''
+Print out, useful statistics if appropriate,
+e.g. running times, averages, etc.
+'''
+def timeprofile(func):
+    def wrapper(im, *args, **kwds):
+        print '- - - - - - - - - - - - - - - - - - - - - - -'
+        print 'Operating on image of size ' + str(im.shape) + '...'
+        before = time()
+        func(im, *args, **kwds)
+        print '  > time to completion: %fs' % (time() - before)
+    return wrapper
+
+
 # 1
+@timeprofile
 def testPainterly(im, texture, outputName, N=10000, size=50, noise=0.3):
     '''
     Using extra debug flags, outputs both the first and second
     passes ONLY of the painterly function. Similar to the handout.
     '''
+    print 'TEST 1: painterly on provided VPD image'
     # uses debug flags as extensions to painterly
     npr.painterly(im, texture, N, size, noise, debug=True, imname=outputName)
 
 
 # 2
-def testForest(N=50000, size=200, noise=0.1):
+@timeprofile
+def testForest(im, N=50000, size=200, noise=0.1):
     '''
     Tests NPR on a beautiful image of a forest I found
     on Google Images.
@@ -61,43 +78,50 @@ def testForest(N=50000, size=200, noise=0.1):
     Generates a squiggly, worm-looking image.
     Quite abstract.
     '''
-    io.imwrite(npr.orientedPaint(forest, bigBrush, N, size, noise),
+    print 'TEST 2: oriented brushing on beautiful forest image'
+    io.imwrite(npr.orientedPaint(im, bigBrush, N, size, noise),
                str("ForestOrientedPaint"+".png"))
 
 
 # 3
-def testForestIntermediate(N=50000, size=200, noise=0.1):
+@timeprofile
+def testForestIntermediate(im, N=50000, size=200, noise=0.1):
     '''
     Verifies the intermediate passes in the painterly function.
     This works on the same forest image as before.
     '''
-    npr.painterly(forest, bigBrush, N, size, noise,
+    print 'TEST 3: painterly on forest (outputting intermediate passes)'
+    npr.painterly(im, bigBrush, N, size, noise,
                   debug=True, imname="Forest")
 
 
 # 4
-def testCatalina(N=50000, size=50, noise=0.2):
+@timeprofile
+def testCatalina(im, N=50000, size=50, noise=0.2):
     '''
     Renders a painted version of Catalina.
     Refer to the macOS Catalina default dynamic wallpaper
     for this.
     '''
-    io.imwrite(npr.orientedPaint(catalina, brush1, N, size, noise),
+    print 'TEST 4: oriented brushing on macOS Catalina wallpaper'
+    io.imwrite(npr.orientedPaint(im, brush1, N, size, noise),
                str("CatalinaOrientedPaint"+".png"))
 
 
 # 5
-def testCatalinaIntermediate(N=50000, size=50, noise=0.2):
+@timeprofile
+def testCatalinaIntermediate(im, N=50000, size=50, noise=0.2):
     '''
     Verifies the intermediate passes in the painterly function.
     This works on the same Catalina image as before in #4.
     '''
-    npr.painterly(catalina, brush1, N, size, noise,
+    print 'TEST 5: painterly on Catalina wallpaper (outputting intermediate passes)'
+    npr.painterly(im, brush1, N, size, noise,
                   debug=True, imname="Catalina")
 
 
 testPainterly(vpd, brush1, "VPD")
-testForest()
-testForestIntermediate()
-testCatalina()
-testCatalinaIntermediate()
+testForest(forest)
+testForestIntermediate(forest)
+testCatalina(catalina)
+testCatalinaIntermediate(catalina)
